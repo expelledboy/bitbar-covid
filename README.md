b2755a6d29e65fe8d379dee5aa1d410884492d9f
expelledboy Anthony Jackson <anthony@betaplum.com>
2020-04-19 10:50:42 +0200

    Ask for location of BitBar plugin directory on install

--- README.md
+++ README.md
@@ -8,6 +8,5 @@ Get updates on covid 19 in your country in your status bar.
 A [BitBar](https://getbitbar.com/) plugin.
 
 ```
-# NOTE: Assuming your plugin directory is located at ~/Documents/BitBar
-curl https://raw.githubusercontent.com/expelledboy/bitbar-covid/master/install.sh | bash
+$ bash <(curl https://raw.githubusercontent.com/expelledboy/bitbar-covid/master/install.sh)
 ```
--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
+#!/bin/bash --
 
 RELEASE=https://raw.githubusercontent.com/expelledboy/bitbar-covid/master/main.py
-SCRIPT=$HOME/Documents/Bitbar/covid.10m.py
+
+read_path () {
+	cd $HOME
+	READ_DIR=$(read -p "> " -re input ; printf "%s" "${input}")
+	eval "${1}"=\"\$\{READ_DIR\}\"
+}
+
+echo "Where is your BitBar Plugin Directory?"
+read_path BITBAR_PLUGIN_DIR
+
+FULL_BITBAR_PLUGIN_DIR=$(cd $HOME/$BITBAR_PLUGIN_DIR && pwd)
+SCRIPT=$FULL_BITBAR_PLUGIN_DIR/covid.10m.py
 
 wget -O $SCRIPT $RELEASE 
 chmod +x $SCRIPT
